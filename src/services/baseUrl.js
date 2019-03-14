b8842defed0bfe9222bd1217a09f48a120841f83
Sonukr <sonuyadav9756@gmail.com>
2019-03-14 11:52:58 +0530

    baseUrl chsnge for prod

--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -23,8 +23,6 @@ import {
       process.env.REACT_APP_BACKEND_SERVER_URL
     ) {
       return process.env.REACT_APP_BACKEND_SERVER_URL;
-    }else if(process.env.NODE_ENV === 'production'){
-       return process.env.REACT_APP_BACKEND_SERVER_URL;
     }else{
         throw new Error(
             'Backend server or REACT_APP_BACKEND_SERVER_URL IS NOT SPECIFIED'
